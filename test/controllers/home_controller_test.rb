1b00eb9ee7199ce0b3f2cf4f534aac8ad1554a93
J-Bandurski <jacek.bandurski@hotmail.co.uk>
2017-11-28 19:20:53 +0000

    Added Testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
+    
+    assert_select 'title', 'My Notes';
+    assert_select 'h1', 'My Notes';
+    assert_select 'p1', 'Welcome to my notes site!';
+    
     assert_response :success
   end
 
