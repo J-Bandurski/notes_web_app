84eca86b11d4e6f65139dfba03de5cbd49ca60fb
J-Bandurski <jacek.bandurski@hotmail.co.uk>
2017-11-29 21:28:46 +0000

    added contact form

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -10,5 +10,34 @@ class HomeControllerTest < ActionController::TestCase
     
     assert_response :success
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    
+    assert_template layout: 'application'
+    
+    assert_select 'title', 'My Notes';
+    assert_select 'h1', 'Contact Us';
+    assert_select 'p1', 'Complete the following form to get in touch with us.';
+  end
+  
+  test "should post request contact but no email" do
+    post :request_contact
+    
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+  
+  test "should post request contact" do
+    post :request_contact,
+      name: "Jacek", email: "jacek@webtestme.com", 
+      telephone: "12345678910", message: "testing"
+    
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
 
 end
