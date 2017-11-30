5f4eb058593dd1aa21bcb89b989051839166bd35
J-Bandurski <jacek.bandurski@hotmail.co.uk>
2017-11-30 01:08:49 +0000

    Model validations and testing

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Should not save empty note" do
+    note = Note.new
+    
+    note.save
+    
+    refute note.valid?
+  end
+  
+  test "Should save non empty note" do
+    note = Note.new
+    
+    note.title = "My Note"
+    note.description = "My Description"
+    
+    note.save
+    
+    assert note.valid?
+  end
+  
+  test "Should not save duplicate note title" do
+    note1 = Note.new
+    note1.title = "My Note"
+    note1.description = "My Description"
+    note1.save
+    assert note1.valid?
+    
+    note2 = Note.new
+    note2.title = "My Note"
+    note2.description = "My Description"
+    note2.save
+    refute note2.valid?
+    
+  end
+  
+
 end
